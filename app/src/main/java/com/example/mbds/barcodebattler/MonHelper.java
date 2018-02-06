4356ac7105ba189e6fad6591eae1884c6017b90c
boucara <traorebelco@gmail.com>
2018-02-06 08:59:39 +0100

    persistance

--- app/src/main/java/com/example/mbds/barcodebattler/MonHelper.java
+++ app/src/main/java/com/example/mbds/barcodebattler/MonHelper.java
@@ -26,19 +26,40 @@ public class MonHelper extends SQLiteOpenHelper {
     private static final String COLUMN_defense = "defense";
     private static final String COLUMN_attaque = "attaque";
     private static final String COLUMN_IMAGE = "image";
+    private static final String COLUMN_EQUIPEMENT="idE";
+
+    private static final String TABLE_NOM = "equipement";
+    private static final String COLUMN_IDE = "idE";
+    private static final String COLUMN_NAME = "nameE";
+    private static final String COLUMN_Point = "point";
+    private static final String COLUMN_attribut = "attribut";
+    private static final String COLUMN_IMAGEE = "imageE";
+    private static final String COLUMN_equipementID  = "attribut";
+
+    private static  final String Table ="CREATE TABLE "+TABLE_NOM+"("+COLUMN_IDE +" "+"INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            "  "+COLUMN_NAME +"  "+ "TEXT NOT NULL,"+
+            "  "+COLUMN_attribut  +"  "+"TEXT NOT NULL ,"+
+            "  "+COLUMN_Point  +"  "+"INTEGER ,"+
+            "  "+COLUMN_IMAGEE +"  "+"BLOB NOT NULL"+");";
+
+
 
     private static  final String CREAT_DB="CREATE TABLE "+TABLE_NAME+"("+COLUMN_ID +" "+"INTEGER PRIMARY KEY AUTOINCREMENT,"+
             "  "+COLUMN_NOM +"  "+ "TEXT NOT NULL,"+
             "  "+COLUMN_PV  +"  "+"INTEGER ,"+
             "  "+COLUMN_defense  +"  "+"INTEGER ,"+
             "  "+COLUMN_attaque  +"  "+"INTEGER ,"+
-            "  "+COLUMN_IMAGE  +"  "+"BLOB NOT NULL"+");";
+            "  "+COLUMN_IMAGE  +"  "+"BLOB NOT NULL"+
+            "  "+COLUMN_equipementID  +"  "+"INTEGER ,"+
+            "   "+COLUMN_EQUIPEMENT+""+" FOREIGN KEY("+COLUMN_equipementID+" ) REFERENCES equipement(COLUMN_IDE),"+");";
+
 
 
 
 
     public MonHelper(Context context) {
         super(context,TABLE_NAME,null,DATABASE_VERSION );
+
     }
 
     @Override
